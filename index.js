82864935ec8ca9bb79a12f2554de062198b720eb
Vladzimir <58780728+vovakopanko@users.noreply.github.com>
2022-05-19 16:50:37 +0300

    add express-static-gzip

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const errorMiddleWare = require("./middleware/error-middleware");
 
 const PORT = process.env.PORT || 8000; // default port , if the variable is empty
 const app = express(); // start our app
-
+const expressStaticGzip = require("express-static-gzip");
 //Middleware
 app.use(express.json()); // parse our json
 app.use(cookieParser()); // Middleware for cookie
@@ -20,7 +20,11 @@ app.use(
 );
 app.use("/api/auth", router); // listen our request
 app.use(errorMiddleWare);
-
+app.use(
+  expressStaticGzip("dist", {
+    enableBrotli: true,
+  })
+);
 const start = async () => {
   try {
     await mongoose.connect(process.env.BD_URL, {
